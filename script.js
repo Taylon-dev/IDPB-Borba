a9f340ed4c01c7432782eee2a9741c62e170bff1
Taylon Silva <taylon.92pellegue@gmail.com>
2025-08-08 13:54:36 -0400

    Tentativa de incluir a animação animar1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function gtoimg1(){
 
+    let r1 = document.getElementById("r1").checked;
+
+    if(r1){
+
+    const imgSlide = document.getElementsByClassName("imgSlide");
+
+    imgSlide.classList.remove("animar1");
+
+    void imgSlide.offsetWidth;
+
+    imgSlide.classList.add("animar1");
+    }
+}
